4243db4976fc1fcf1ed02d2b5fc6e0376a300920
tdelort <tristan.delort@gmail.com>
2022-04-22 19:51:08 -0400

    OpenGL setup

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # GlVoronoi
 
 Implementation of some Voronoi algorithms using C++ and OpenGL.
+
+## Things i read during the implementation
+
+<https://nullprogram.com/blog/2014/06/01/>
+
+Nice idea to use the Cones but I'm looking for an exact implementation in 2D only since it will be used to generate maps procedurally.
--- demo.cpp
+++ demo.cpp
@@ -11,12 +11,58 @@
 // GLFW
 #include <GLFW/glfw3.h> // Will drag system OpenGL headers
 
-int main(int, char**)
+// STD LIBS
+#include <iostream>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 800
+
+#define MAX_SEED_COUNT 128
+
+#define _DEBUG_SHADER
+
+static const char* vertexSource = R"glsl(
+    #version 150
+
+	in vec3 position;
+
+    out vec3 fragPos;
+
+	void main()
+	{
+		gl_Position = vec4(position, 1.0);
+        fragPos = vec3(position);
+	}
+)glsl";
+
+static const char* fragmentSource = R"glsl(
+    #version 150
+
+    in vec3 fragPos;
+
+    uniform vec2 u_resolution;
+
+	out vec4 outColor;
+
+	void main()
+	{
+        vec2 uv = fragPos.xy * 0.5 + 0.5;
+        vec2 pixelCoord = uv * u_resolution;
+		outColor = vec4(pixelCoord / u_resolution, 0.0, 1.0);
+	}
+)glsl";
+
+static void glfw_error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
+
+GLFWwindow *InitGUI()
 {
     // Setup window
     glfwSetErrorCallback(glfw_error_callback);
     if (!glfwInit())
-        return 1;
+        exit(1);
 
     // Decide GL+GLSL versions
     // GL 3.0 + GLSL 150
@@ -27,9 +73,9 @@ int main(int, char**)
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
 
     // Create window with graphics context
-    GLFWwindow* window = glfwCreateWindow(720, 720, "Dear ImGui GLFW+OpenGL3 example", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Dear ImGui GLFW+OpenGL3 example", NULL, NULL);
     if (window == NULL)
-        return 1;
+        exit(1);
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1); // Enable vsync
 
@@ -37,12 +83,9 @@ int main(int, char**)
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
     // Setup Dear ImGui style
     ImGui::StyleColorsDark();
-    //ImGui::StyleColorsClassic();
 
     // Setup Platform/Renderer backends
     ImGui_ImplGlfw_InitForOpenGL(window, true);
@@ -54,75 +97,12 @@ int main(int, char**)
         exit(1);
     }
 
-    // Our state
-    bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    return window;
+}
 
-    // Main loop
-    while (!glfwWindowShouldClose(window))
-    {
-        // Poll and handle events (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
-        glfwPollEvents();
 
-        // Start the Dear ImGui frame
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
-
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
-
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
-        {
-            static float f = 0.0f;
-            static int counter = 0;
-
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            ImGui::End();
-        }
-
-        // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
-        }
-
-        // Rendering
-        ImGui::Render();
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-        glfwSwapBuffers(window);
-    }
-
+void CleanupGUI(GLFWwindow *window)
+{
     // Cleanup
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
@@ -130,6 +110,137 @@ int main(int, char**)
 
     glfwDestroyWindow(window);
     glfwTerminate();
+}
+
+GLuint CreateShaderProgram()
+{
+    // Creating program (shaders)
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexSource, NULL);
+    glCompileShader(vertexShader);
+#ifdef _DEBUG_SHADER
+    GLint status;
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
+    std::cout << "Vertex shader compile status : " << status << " (1 means success)" << std::endl;
+    if(status == GL_FALSE)
+    {
+        GLint infoLogLength;
+        glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &infoLogLength);
+        GLchar *strInfoLog = new GLchar[infoLogLength + 1];
+        glGetShaderInfoLog(vertexShader, infoLogLength, NULL, strInfoLog);
+        std::cout << "Compile failure in vertex shader : " << std::endl;
+        std::cout << strInfoLog << std::endl;
+        delete[] strInfoLog;
+    }
+#endif
+
+    // Creating Fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+    glCompileShader(fragmentShader);
+#ifdef _DEBUG_SHADER
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
+    std::cout << "Fragment shader compile status : " << status << " (1 means success)" << std::endl;
+    if(status == GL_FALSE)
+    {
+        GLint infoLogLength;
+        glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &infoLogLength);
+        GLchar *strInfoLog = new GLchar[infoLogLength + 1];
+        glGetShaderInfoLog(fragmentShader, infoLogLength, NULL, strInfoLog);
+        std::cout << "Compile failure in fragment shader : " << std::endl;
+        std::cout << strInfoLog << std::endl;
+        delete[] strInfoLog;
+    }
+#endif
+
+    // Creating shader program and linking it
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+    glLinkProgram(program);
+    glUseProgram(program);
+    return program;
+}
+
+int main(int, char**)
+{
+    GLFWwindow *window = InitGUI();
+
+    GLuint program = CreateShaderProgram();
+
+    // ----- creating a plane -----
+    GLfloat planeVertex[] = {
+        -1, 1, 0,
+        1, 1, 0,
+        -1, -1, 0,
+        1, -1, 0
+    };
+    GLuint planeIndices[] = {
+        0, 1, 2,
+        1, 2, 3
+    };
+    GLuint planeVBO, planeVAO, planeEBO;
+    glGenVertexArrays(1, &planeVAO);
+    glBindVertexArray(planeVAO);
+
+    glGenBuffers(1, &planeVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertex), planeVertex, GL_STATIC_DRAW);
+
+    GLint positionAttrib = glGetAttribLocation(program, "position");
+    glVertexAttribPointer(positionAttrib, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(positionAttrib);
+
+    glGenBuffers(1, &planeEBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, planeEBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(planeIndices), planeIndices, GL_STATIC_DRAW);
+
+    // Uniforms
+    GLint screenSizeUniform = glGetUniformLocation(program, "u_resolution");
+    //GLint seedCountUniform = glGetUniformLocation(program, "seedCount");
+    //GLint seedsPositionUniform = glGetUniformLocation(program, "seedsPosition");
+    //GLint seedsColorUniform = glGetUniformLocation(program, "seedsColor");
+    // ----- plane created -----
+
+    // Our state
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    int num_seeds = 0;
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        {
+            // IMGUI WINDOW HERE
+        }
+
+
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        
+        // Clear
+        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw
+        glBindVertexArray(planeVAO);
+        glUniform2f(screenSizeUniform, display_w, display_h);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        // Show
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+    }
+
 
     return 0;
 }
